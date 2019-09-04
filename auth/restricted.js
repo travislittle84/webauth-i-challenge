1b20fc6b8190ec40566195e44dcccab662e894b2
Travis Little <kingtrav@gmail.com>
2019-09-04 17:48:50 -0600

    created restricted.js and auth folder to auth user

---
+++ auth/restricted.js
@@ -0,0 +1,23 @@
+const Users = require('../users/users-model.js')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    restricted
+}
+
+function restricted(req, res, next) {
+    const { username, password } = req.headers
+    if (username && password) {
+        Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+            } else {
+                res.status(401).json({ message: "You shall not pass!" })
+            }
+        })
+    } else {
+        res.status(400).json({ message: 'Please provide username and password' })
+    }
+}
